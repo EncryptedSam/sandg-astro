2be9b80640ddb5faf05e3eb4c02cb89230c33874
EncryptedSam <sampathkumar.ajjapaga@gmail.com>
2026-05-24 00:02:44 +0530

    lightbox v2

--- src/components/ui/LightboxGalleryV2.ts
+++ src/components/ui/LightboxGalleryV2.ts
@@ -1,11 +1,10 @@
 export function openLightbox(
+  id: string,
   data: { src: string }[],
   index: number,
   title?: string,
 ) {
   window.dispatchEvent(
-    new CustomEvent("open-lightbox", { detail: { data, index, title } }),
+    new CustomEvent("open-lightbox", { detail: { data, index, title, id } }),
   );
 }
-
-// openLightbox({}[]);
